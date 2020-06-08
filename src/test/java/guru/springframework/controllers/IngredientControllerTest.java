1d14bb9449580445f4101b4a6ab86700a23e2383
bartoszjarzab <bartosz.jarzab@gmail.com>
2020-06-08 11:49:35 +0200

    added update ingredient view, show, and update forms

--- src/test/java/guru/springframework/controllers/IngredientControllerTest.java
+++ src/test/java/guru/springframework/controllers/IngredientControllerTest.java
@@ -1,14 +1,20 @@
 package guru.springframework.controllers;
 
+import guru.springframework.commands.IngredientCommand;
 import guru.springframework.commands.RecipeCommand;
+import guru.springframework.services.IngredientService;
 import guru.springframework.services.RecipeService;
+import guru.springframework.services.UnitOfMeasureService;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
 
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
@@ -21,10 +27,16 @@ public class IngredientControllerTest {
     @Mock
     RecipeService recipeService;
 
+    @Mock
+    IngredientService ingredientService;
+
+    @Mock
+    UnitOfMeasureService unitOfMeasureService;
+
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        controller=new IngredientController(recipeService);
+        controller=new IngredientController(recipeService,ingredientService,unitOfMeasureService);
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
 
     }
@@ -41,4 +53,55 @@ public class IngredientControllerTest {
                 .andExpect(model().attributeExists("recipe"));
         verify(recipeService,times(1)).findCommandById(anyLong());
     }
+
+    @Test
+    public void showIngredient() throws Exception {
+        IngredientCommand ingredientCommand = new IngredientCommand();
+
+
+        when(ingredientService.findByRecipeIdAndIngredientId(anyLong(),anyLong())).thenReturn(ingredientCommand);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/recipe/1/ingredient/2/show"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/ingredient/show"))
+                .andExpect(model().attributeExists("ingredient"));
+        verify(ingredientService,times(1)).findByRecipeIdAndIngredientId(anyLong(),anyLong());
+    }
+
+    @Test
+    public void testUpdateIngredientForm() throws Exception {
+
+        IngredientCommand ingredientCommand = new IngredientCommand();
+
+        when(ingredientService.findByRecipeIdAndIngredientId(anyLong(),anyLong())).thenReturn(ingredientCommand);
+        when(unitOfMeasureService.listAllUoms()).thenReturn(new HashSet<>());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/recipe/1/ingredient/2/update"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/ingredient/ingredientform"))
+                .andExpect(model().attributeExists("ingredient"))
+                .andExpect(model().attributeExists("uomList"));
+    }
+
+    @Test
+    public void testSaveOrUpdate() throws Exception{
+        //given
+        IngredientCommand command = new IngredientCommand();
+        command.setId(3L);
+        command.setRecipeId(2L);
+
+        //when
+        when(ingredientService.saveOrUpdateIngredientCommand(any())).thenReturn(command);
+
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.post("/recipe/2/ingredient")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id","")
+                .param("description", "some string")
+        )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/recipe/2/ingredient/3/show"));
+
+    }
+
 }
---
+++ src/test/java/guru/springframework/services/UnitOfMeasureServiceImplTest.java
@@ -0,0 +1,59 @@
+package guru.springframework.services;
+
+import guru.springframework.commands.UnitOfMeasureCommand;
+import guru.springframework.converters.UnitOfMeasureToUnitOfMeasureCommand;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.UnitOfMeasureRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class UnitOfMeasureServiceImplTest {
+
+    UnitOfMeasureService unitOfMeasureService;
+
+    UnitOfMeasureToUnitOfMeasureCommand unitOfMeasureToUnitOfMeasureCommand=new UnitOfMeasureToUnitOfMeasureCommand();
+
+    @Mock
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        unitOfMeasureService = new UnitOfMeasureServiceImpl(unitOfMeasureRepository,unitOfMeasureToUnitOfMeasureCommand);
+
+    }
+
+    @Test
+    public void listAllUoms() {
+        //given
+        Set<UnitOfMeasure> unitOfMeasures = new HashSet<>();
+        UnitOfMeasure uom1=new UnitOfMeasure();
+        uom1.setId(1L);
+        unitOfMeasures.add(uom1);
+
+        UnitOfMeasure uom2=new UnitOfMeasure();
+        uom1.setId(2L);
+        unitOfMeasures.add(uom2);
+
+        //when
+        when(unitOfMeasureRepository.findAll()).thenReturn(unitOfMeasures);
+
+        //then
+        Set<UnitOfMeasureCommand> commands = unitOfMeasureService.listAllUoms();
+        assertEquals(2,commands.size());
+        verify(unitOfMeasureRepository,times(1)).findAll();
+
+
+
+
+
+    }
+}
